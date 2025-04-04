3b4393da0f74ac521d31fae7aad94d109cfd3a59
LauraKastenn <laura_kasten@estudante.sesisenai.org.br>
2025-04-04 12:39:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//atv 4
+let numero1 = prompt("Digite um numero")
+let numero2 = prompt("Digite outro numero")
+let numero3 = prompt("Digite outro numero")
+let numero4 = prompt("Digite outro numero")
+let count = numero1;
+
+while (count >= 1) {
+    console.log(count)
+    count--;
+}
+let countt = numero2;
+while (countt >= 1) {
+    console.log(countt)
+    countt--;
+} 
+let ccount = numero3;
+while (ccount >= 1) {
+    console.log(ccount)
+    ccount--;
+}
+
+let ccountt = numero4;
+while (ccountt >= 1) {
+    console.log(ccountt)
+    ccountt--;
+} 
+
+//atv 6
+let array = [10, 20, 30, 40, 50];
+console.log(array[0] + array[1] + array[2] + array[3] + array[4])
+
+
+
